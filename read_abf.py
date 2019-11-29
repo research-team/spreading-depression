c6942193d481e46937f4dde63e6729629aeb39f0
Max Talanov <max.talanov@gmail.com>
2019-11-29 19:52:22 +0300

    completed reading abf file example

--- read_abf.py
+++ read_abf.py
@@ -1,10 +1,23 @@
 import neo
+import numpy as np
 import logging
+from matplotlib import pyplot as plt
+
 
 logging.basicConfig(format='[%(funcName)s]: %(message)s', level=logging.INFO)
 logger = logging.getLogger()
 
 filename = '../19n28000.abf'
 r = neo.io.AxonIO(filename)
-blk = r.read_block()
-logger.info(blk)
+block = r.read_block()
+logger.info(block)
+
+for seg in block.segments:
+    print("Analyzing segment %d" % seg.index)
+
+    avg = np.mean(seg.analogsignals[0], axis=1)
+
+    plt.figure()
+    plt.plot(avg)
+    plt.title("Peak response in segment %d: %f" % (seg.index, avg.max()))
+    plt.show()
