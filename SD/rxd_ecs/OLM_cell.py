66e30b07fb3e495cd00009cfe75ed03db83c018c
KseniiaKarpova <ksusha.karpova2011@yandex.ru>
2020-04-02 21:05:32 +0300

    up to 40mv

--- SD/rxd_ecs/OLM_cell.py
+++ SD/rxd_ecs/OLM_cell.py
@@ -13,8 +13,19 @@ import numpy as np
 from neuron.units import ms, mV
 from matplotlib import pyplot, animation
 from IPython.display import HTML
+import matplotlib.pyplot as plt
+from neuron import gui
+'''
 
-# mpiexec -n 1 nrniv -mpi -python cell_sd.py
+    Oriens-lacunosum moleculare (OLM) cells are a major subclass of hippocampal interneurons 
+    involved in controlling synaptic plasticity in Shaffer collateral synapses 
+    and electrogenesis in pyramidal cell (PC) dendrites. 
+
+see more in here: https://www.frontiersin.org/articles/10.3389/fncel.2015.00201/full
+
+for run : mpiexec -n 1 nrniv -mpi -python OLM_cell.py 
+'''
+
 pc = h.ParallelContext()
 pcid = pc.id()
 nhost = pc.nhost()
@@ -27,15 +38,7 @@ h.load_file('stdrun.hoc')
 h.celsius = 37
 numpy.random.seed(6324555+pcid)
 
-
-
-
-
-outdir = os.path.abspath('tests/311')
-
-
-
-
+outdir = os.path.abspath('tests/305')
 
 
 k_na_dir = os.path.abspath(os.path.join(outdir, 'K_NA'))
@@ -56,20 +59,25 @@ ihscale=1.0
 erevh = -30.0
 
 
-somaR = 6.0  
-dendR = 1.4  
-dendL = 50.0  
-doff = dendL + somaR
+somaR = 5
+somaL = 20  
+dendR = 1.5  
+dendL = 250 
+axonL = 150
+axonR =  1.5/2
+
 alpha = 0.2  
 tort = 1.6  
-r0 = 60
+r0 = 1000
 x, y, z =0, 0, 30
-Lx, Ly, Lz = 100, 100, 100 
+Lx, Ly, Lz = 1500, 1500, 1000 
 Kceil = 15.0 
 
-Rm = 28000 # Ohm.cm^2 (Migliore value)
+Rm = 1/5e-05  # Ohm.cm^2 (Migliore value) or 20000*2
 gka_soma = 0.0075
 gh_soma  = 0.00005
+Ra = 150
+cm = 1.6
 
 class Neuron:
 
@@ -77,18 +85,45 @@ class Neuron:
 
         self.soma = h.Section(name='soma', cell=self)
         self.soma.pt3dclear()
-        self.soma.pt3dadd(x, y, z + somaR, 2.0 * somaR)
-        self.soma.pt3dadd(x, y, z - somaR, 2.0 * somaR)
+        self.soma.pt3dadd(0,0,0,10)
+        self.soma.pt3dadd(15,0,0,10)
+        self.soma.L = 20
+        self.soma.diam = 10
 
-        self.dend = h.Section(name='dend', cell=self)
-        self.dend.pt3dclear()
-        self.dend.pt3dadd(x, y, z - somaR, 2.0 * dendR)
-        self.dend.pt3dadd(x, y, z - somaR - dendL, 2.0 * dendR)
-        self.dend.nseg = 10
+        self.dend1 = h.Section(name='dend1', cell=self)
+        self.dend1.pt3dclear()
+        self.dend1.pt3dadd(15,0,0,3)
+        self.dend1.pt3dadd(90,0,0,3)
+        self.dend1.L = 250
+        self.dend1.diam = 3
+        self.dend1.nseg = 10
+        self.dend1.connect(self.soma, 1, 0)
 
-        self.dend.connect(self.soma, 1, 0)
-        self.all = [self.soma, self.dend]
+        self.dend2 = h.Section(name='dend2', cell=self)
+        self.dend2.pt3dclear()
+        self.dend2.pt3dadd(0,0,0,3)
+        self.dend2.pt3dadd(-74,0,0,3)
+        self.dend2.L = 250
+        self.dend2.diam = 3
+        self.dend2.nseg = 10
+        self.dend2.connect(self.soma, 0, 0)
 
+        self.axon = h.Section(name='axon', cell=self)
+        self.axon.pt3dclear()
+        self.axon.pt3dadd(15,0,0,1.3)
+        self.axon.pt3dadd(15,120,0,1.5)
+        self.axon.L = 150
+        self.axon.diam = 1.5
+        self.axon.nseg = 10
+        self.axon.connect(self.soma, 1, 0)
+
+        
+        h.topology()
+        #h.PlotShape(False).plot(plt)
+        
+
+        self.all = [self.soma, self.dend1, self.dend2, self.axon]
+        print(1)
         #---------------soma----------------
         for mechanism_s in [ 'IA', 'Ih','Nasoma','Ksoma']:
             self.soma.insert(mechanism_s)
@@ -106,59 +141,77 @@ class Neuron:
 
         self.somaV = h.Vector()
         self.somaV.record(self.soma(0.5)._ref_v)
-        
-        #---------------dend----------------
-        
-        for mechanism_d in [ 'IA','Nadend','Kdend', 'nap', 'kap', 'hNa', 'Nafcr', 'IKscr', 'km']:
-            self.dend.insert(mechanism_d)
+        '''
+            {'point_processes': {}, 'density_mechs': {
+            'IA': {'gkAbar': [0.0165], 'ik': [0.0], 'a': [0.0], 'b': [0.0]}, 
+            'Ih': {'gkhbar': [3.5000000000000004e-05], 'ih': [0.0], 'r': [0.0]}, 
+            'Ksoma': {'gksoma': [0.04785], 'ik': [0.0], 'n': [0.0]}, 
+            'Nasoma': {'gnasoma': [0.012839999999999999], 'gl': [5e-05], 'el': [-67.0], 'ina': [0.0], 'il': [0.0], 'm': [0.0], 'h': [0.0]}}, 'ions': {'na': {'ena': [50.0], 'nai': [10.0], 'nao': [140.0], 'ina': [0.0], 'dina_dv_': [0.0]}, 'k': {'ek': [-77.0], 'ki': [54.4], 'ko': [2.5], 'ik': [0.0], 'dik_dv_': [0.0]}, 'h': {'eh': [0.0], 'hi': [1.0], 'ho': [1.0], 'ih': [0.0], 'dih_dv_': [0.0]}}, 'morphology': {'L': 20.0, 'diam': [10.0], 'pts3d': [(0.0, 0.0, 0.0, 10.0), (20.0, 0.0, 0.0, 10.0)], 'parent': None, 'trueparent': None}, 'nseg': 1, 'Ra': 35.4, 'cm': [1.0], 'regions': set(), 'species': set(), 'name': '<__main__.Neuron object at 0x7faabfacc048>.soma', 'hoc_internal_name': '__nrnsec_0x5628921c9100', 'cell': <__main__.Neuron object at 0x7faabfacc048>}
 
-        self.dend(0.5).IA.gkAbar = 0.004*1.2
+         '''
+        print(2)
+        #---------------dends----------------
+        for mechanism_d in [ 'IA','Nadend','Kdend']:
+            self.dend1.insert(mechanism_d)
+            self.dend2.insert(mechanism_d)
+#-----------------------dend1------------------------
+        self.dend1(0.5).IA.gkAbar = 0.004*1.2
         #self.dend1(0.5).Ih.gkhbar = 0.00035*0.1
-        self.dend(0.5).Nadend.gnadend = 2*0.0117
-        self.dend(0.5).Nadend.gl = 1/Rm
-        self.dend(0.5).Nadend.el = -65
-        self.dend(0.5).Kdend.gkdend = 20*0.023
+        self.dend1(0.5).Nadend.gnadend = 2*0.0117
+        self.dend1(0.5).Nadend.gl = 1/Rm
+        self.dend1(0.5).Nadend.el = -65
+        self.dend1(0.5).Kdend.gkdend = 20*0.023
 
-        self.Dn_Kdend = h.Vector().record(self.dend(0.5).Kdend._ref_n)
-        self.Dh_Nadend = h.Vector().record(self.dend(0.5).Nadend._ref_h)
-        self.Dm_Nadend = h.Vector().record(self.dend(0.5).Nadend._ref_m)
-        self.dendV = h.Vector()
-        self.dendV.record(self.dend(0.5)._ref_v)
-        self.k_vec = h.Vector().record(self.dend(0.5)._ref_ik)
-        self.na_vec = h.Vector().record(self.dend(0.5)._ref_ina)
-        #print(numpy.array(self.k_i))
-        #print(nu mpy.array(self.k.nodes.concentration)) 11 count
-        self.na_concentration = h.Vector().record(self.dend(0.5)._ref_nai)
-        self.k_concentration = h.Vector().record(self.dend(0.5)._ref_ki)
+        self.D1n_Kdend = h.Vector().record(self.dend1(0.5).Kdend._ref_n)
+        self.D1h_Nadend = h.Vector().record(self.dend1(0.5).Nadend._ref_h)
+        self.D1m_Nadend = h.Vector().record(self.dend1(0.5).Nadend._ref_m)
+        self.dendV1 = h.Vector()
+        self.dendV1.record(self.dend1(0.5)._ref_v)
+#-----------------------dend2------------------------
+        self.dend2(0.5).IA.gkAbar = 0.004*1.2
+        #self.dend1(0.5).Ih.gkhbar = 0.00035*0.1
+        self.dend2(0.5).Nadend.gnadend = 2*0.0117
+        self.dend2(0.5).Nadend.gl = 1/Rm
+        self.dend2(0.5).Nadend.el = -65
+        self.dend2(0.5).Kdend.gkdend = 20*0.023
+
+        self.D2n_Kdend = h.Vector().record(self.dend2(0.5).Kdend._ref_n)
+        self.D2h_Nadend = h.Vector().record(self.dend2(0.5).Nadend._ref_h)
+        self.D2m_Nadend = h.Vector().record(self.dend2(0.5).Nadend._ref_m)
+
+        self.dendV2 = h.Vector()
+        self.dendV2.record(self.dend2(0.5)._ref_v)
+        print(3)
+        #--------------axon-----------------
+        for mechanism in [ 'Kaxon', 'Naaxon']:
+            self.axon.insert(mechanism)
+
+        self.axon(0.5).Naaxon.gnaaxon = 0.01712
+        self.axon(0.5).Naaxon.gl = 1/Rm
+        self.axon(0.5).Naaxon.el = -67
+        self.axon(0.5).Kaxon.gkaxon = 0.05104
+        #print(self.soma.psection())
+        self.n_Kaxon = h.Vector().record(self.axon(0.5).Kaxon._ref_n)
+        self.h_Naaxon = h.Vector().record(self.axon(0.5).Naaxon._ref_h)
+        self.m_Naaxon = h.Vector().record(self.axon(0.5).Naaxon._ref_m)
+
+        self.axonV = h.Vector()
+        self.axonV.record(self.axon(0.5)._ref_v)
+
+        
+
+        self.k_vec = h.Vector().record(self.soma(0.5)._ref_ik)
+        self.na_vec = h.Vector().record(self.soma(0.5)._ref_ina)
+        self.na_concentration = h.Vector().record(self.soma(0.5)._ref_nai)
+        self.k_concentration = h.Vector().record(self.soma(0.5)._ref_ki)
         
         
         for sec in self.all:        
             Ra = 150
             cm = 1
-
+        print(4)
 sys.stdout.write('\nrun')
 sys.stdout.flush()
-
-
-# show ion in extracellular space  
-def video(species, min_conc=3, max_conc=40, frames=200):
-    
-    fig = pyplot.figure()
-    im = pyplot.imshow(species[ecs].states3d.mean(2), vmin=min_conc, vmax=max_conc)
-    pyplot.axis('off')
-
-    def init():
-        im.set_data(species[ecs].states3d.mean(2))
-        return [im]
-    def animate(i):
-        h.fadvance()
-        im.set_data(species[ecs].states3d.mean(2))
-        return [im]
-
-    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=frames, interval=300)
-    ret =  HTML(anim.to_html5_video())
-    anim.save(os.path.join(outdir,'basic_animation.mp4'))
-    pyplot.close()
 
 
 
@@ -174,25 +227,20 @@ stim.dur = 50
 stim.amp = 1
 '''
 time = h.Vector().record(h._ref_t)
-
+print(5)
 ecs = rxd.Extracellular(-Lx/2.0, -Ly/2.0,
                         -Lz/2.0, Lx/2.0, Ly/2.0, Lz/2.0, dx=1,
                         volume_fraction=alpha, tortuosity=tort) 
-
+print(6)
 k = rxd.Species(ecs, name='k', d=2.62, charge=1, initial=lambda nd: 50 
                 if nd.x3d**2 + nd.y3d**2 + nd.z3d**2 < r0**2 else 3,
                 ecs_boundary_conditions=3)
 
 na = rxd.Species(ecs, name='na', d=1.78, charge=1, initial=148,
                  ecs_boundary_conditions=148)
-print(2)
-# points for record concetration
-#k_0_0_0=h.Vector().record(k[ecs].node_by_location(0, 0, 0)._ref_value)
-#k_10_10_20=h.Vector().record(k[ecs].node_by_location(10, 10, -20)._ref_value)
-#k_20_20_50=h.Vector().record(k[ecs].node_by_location(17, 17, -48)._ref_value)
-print(3)
 
-h.finitialize(-65 * mV)
+pc.set_maxstep(100)
+h.finitialize()
 
 sys.stdout.write('\ninit')
 sys.stdout.flush()
@@ -205,56 +253,7 @@ def progress_bar(tstop, size=40):
     sys.stdout.write('[%s] %2.1f%% %6.1fms of %6.1fms\r' % (progress, 100 * prog, pc.t(0), tstop))
     sys.stdout.flush()
 
-def plot_rec_neurons():
-    somaV, dendV, pos = [], [], []
-    
-    fin = open(os.path.join(outdir,'membrane_potential.pkl'),'rb')
-    [sV, dV, p] = pickle.load(fin)
-    fin.close()
-    somaV.extend(sV) #list
-    dendV.extend(dV) #list
-    pos.extend(p) #list.len()=3
 
-    for idx in range(len(somaV)):
-        if idx%100==0:  
-            fig = pyplot.figure()
-            ax = fig.add_subplot(111,projection='3d')
-            ax.set_position([0.0,0.05,0.9,0.9])
-            ax.set_xlim([-Lx/2.0, Lx/2.0])
-            ax.set_ylim([-Ly/2.0, Ly/2.0])
-            ax.set_zlim([-Lz/2.0, Lz/2.0])
-            
-
-            cmap = pyplot.get_cmap('jet')
-            
-            x = pos
-            soma_z = [x[2]-somaR,x[2]+somaR]
-            cell_x = [x[0],x[0]]
-            cell_y = [x[1],x[1]]
-            scolor = cmap((somaV[idx]+70.0)/70.0)
-            # plot the soma
-            ax.plot(cell_x, cell_y, soma_z, linewidth=2, color=scolor, 
-                    alpha=0.5)
-
-            dcolor = cmap((dendV[idx]+70.0)/70.0)
-            dend_z = [x[2]-somaR, x[2]-somaR - dendL]
-            # plot the dendrite
-            ax.plot(cell_x, cell_y, dend_z, linewidth=0.5, color=dcolor, 
-                    alpha=0.5)
-
-            norm = colors.Normalize(vmin=-70,vmax=0)
-            pyplot.title('Neuron membrane potentials; t = %gms' % (idx * 100))
-
-            # add a colorbar 
-            ax1 = fig.add_axes([0.88,0.05,0.04,0.9])
-            cb1 = colorbar.ColorbarBase(ax1, cmap=cmap, norm=norm,
-                                                orientation='vertical')
-            cb1.set_label('mV')
-            
-            # save the plot
-            filename = 'neurons_{:05d}.png'.format(idx)
-            pyplot.savefig(os.path.join(outdir,filename))
-            pyplot.close()
 
 def plot_spike(volt_soma, volt_dend, t, k, na, k_in, na_in):
 
@@ -286,83 +285,15 @@ def plot_spike(volt_soma, volt_dend, t, k, na, k_in, na_in):
 
 
 
-def plot_K_ecs_in_points(x, t):
-    for i in x:
-        pyplot.plot(t, i)
-    pyplot.legend(['0 0 0','10 10 -20','20 20 -50'])
-    pyplot.grid()
-    pyplot.savefig(os.path.join(outdir, 'k_ecs.png'))
-    pyplot.close('all')
 
 
-
-def plot_n_m_h(t, soma):
-    fig = pyplot.figure(figsize=(20,16))
-    ax1 = fig.add_subplot(3,1,1)
-    ax1.plot(t, soma.n_km, label='km')
-    ax1.plot(t, soma.n_kap, label='kap')
-    ax1.plot(t, soma.n_hha2, label='hha2')
-    ax1.legend()
-    ax1.set_ylabel('state')
-
-    ax2 = fig.add_subplot(3,1,2)
-    ax2.plot(t, soma.m_hha2, label='hha2')
-    ax2.plot(t, soma.m_kca, label='kca')
-    ax2.legend()
-    ax2.set_ylabel('state')
-
-    ax3 = fig.add_subplot(3,1,3)
-    ax3.plot(t , soma.h_hha2 , label='hha2')
-    ax3.legend()
-    ax3.set_ylabel('state')
-
-    
-
-
-    ax3.set_xlabel('time (ms)')
-    fig.savefig(os.path.join(nmh_dir, 'nmhSOMA.png'))
-    pyplot.close('all')
-
-def plot_n_m_h_Dend(t, soma):
-    fig = pyplot.figure(figsize=(20,16))
-    ax1 = fig.add_subplot(3,1,1)
-    ax1.plot(t, soma.Dn_km, label='km')
-    ax1.plot(t, soma.Dn_kap, label='kap')
-    ax1.plot(t, soma.Dn_hha_old, label='hha_old')
-    ax1.plot(t, soma.Dn_Kdend, label='Kdend')
-    ax1.legend()
-    ax1.set_ylabel('state')
-
-    ax2 = fig.add_subplot(3,1,2)
-    ax2.plot(t, soma.Dm_kca, label='kca')
-    ax2.plot(t, soma.Dm_cat, label='cat')
-    ax2.plot(t, soma.Dm_car, label='car')
-    ax2.plot(t, soma.Dm_hha_old, label='hha_old')
-    ax2.plot(t, soma.Dm_calH, label='calH')
-    ax2.plot(t, soma.Dm_Nadend, label='Nadend')
-    ax2.plot(t, soma.Dm_nap, label='nap')
-    ax2.legend()
-    ax2.set_ylabel('state')
-
-    ax3 = fig.add_subplot(3,1,3)
-    ax3.plot(t , soma.Dh_hha_old , label='hha_old')
-    ax3.plot(t , soma.Dh_cat , label='cat')
-    ax3.plot(t , soma.Dh_car , label='car')
-    ax3.plot(t , soma.Dh_calH , label='calH')
-    ax3.plot(t , soma.Dh_Nadend , label='Nadend')
-    ax3.legend()
-    ax3.set_ylabel('state')
-
-    ax3.set_xlabel('time (ms)')
-    fig.savefig(os.path.join(nmh_dir, 'nmhDEND.png'))
-    pyplot.close('all')
 
 
 def plot_image_data(data, min_val, max_val, filename, title):
     """Plot a 2d image of the data"""
     sb = scalebar.ScaleBar(1e-6)
     sb.location='lower left'
-    pyplot.imshow(data, extent=k[ecs].extent('xz'), vmin=min_val,
+    pyplot.imshow(data, extent=k[ecs].extent('xy'), vmin=min_val,
                   vmax=max_val, interpolation='nearest', origin='lower')
     pyplot.colorbar()
     sb = scalebar.ScaleBar(1e-6)
@@ -373,7 +304,7 @@ def plot_image_data(data, min_val, max_val, filename, title):
     ax.add_artist(sb)
     pyplot.title(title)
     pyplot.xlim(k[ecs].extent('x'))
-    pyplot.ylim(k[ecs].extent('z'))
+    pyplot.ylim(k[ecs].extent('y'))
     pyplot.savefig(os.path.join(outdir,filename))
     pyplot.close()
 
@@ -426,7 +357,6 @@ def plot_cell( filename):
 h.dt=1
 
 def run(tstop):
-    print(1)
     if pcid == 0:
         fout = open(os.path.join(outdir,'wave_progress.txt' ),'a')
     while pc.t(0) <= tstop:
@@ -452,35 +382,30 @@ def run(tstop):
             fout.write("%g\t%g\t%g\n" %(pc.t(0), dist, dist1))
             fout.flush()
     #plot_image_region(cell.k.nodes.concentration, 2.5, 140, 'Potassium intracellular; t = %6.0fms' % h.t, cell.k, cell.cyt)
-    
+    print(7)
     sys.stdout.write('\ndone, wait\n')
     sys.stdout.flush()
     
     #progress_bar(tstop)
-    
+    print(8)
     if pcid == 0:
     #plot_image_region(cell.k.nodes.concentration, 2.5, 140, 'Potassium intracellular; t = %6.0fms' % h.t, cell.k, cell.cyt)
         fout.close()
-    
         plot_spike(cell.somaV,
-                    cell.dendV,
+                    cell.dend1V,
                     time,
                     cell.k_vec,
                     cell.na_vec,
                     cell.k_concentration,
                     cell.na_concentration)
-        #plot_n_m_h(time, cell)
-        #plot_n_m_h_Dend(time, cell)
-        sys.stdout.write('Simulation complete. Plotting membrane potentials')
-        sys.stdout.flush()
-       # plot_K_ecs_in_points([k_0_0_0,k_10_10_20, k_20_20_50 ] ,time)
+    
+    
     pos = [x, y, z]
     pout = open(os.path.join(outdir,"membrane_potential.pkl"),'wb')
     pickle.dump([cell.somaV, cell.dendV, pos],pout)
     pout.close()
     pc.barrier()
-    plot_cell('cell')    
-    if pcid == 0: plot_rec_neurons()
+    #plot_cell('cell')    
     exit(0)
 
 run(200)
