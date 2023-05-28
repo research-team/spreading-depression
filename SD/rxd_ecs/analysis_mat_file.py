375667285b43675c52c7953fb2e152a95cc83e45
Kapoline <polina.chit@mail.ru>
2023-05-28 15:36:29 +0300

    update analysis and lfp

--- SD/rxd_ecs/analysis_mat_file.py
+++ SD/rxd_ecs/analysis_mat_file.py
@@ -22,32 +22,29 @@ data = load_data(filepath)
 color = ['red', 'blue', 'yellow', 'black', 'gray', 'cyan', 'magenta']
 
 fig, ax = plt.subplots()
-bins = np.arange(0.9, 10, 0.1)
+bins = np.arange(0.1, 100, 0.1)
 
 def analysis(spks):
-    ISIS=np.concatenate(spks)
-    counts, _ = histogram(ISIS, bins)
-    prob = counts / len(ISIS)
-    bar(bins[:-1], prob, width=0.1)
-    lbda = 1 / ISIS.mean()
+    arr=np.concatenate(spks)
+    ISIs=np.diff(arr)
+    counts, _ = histogram(ISIs, bins)
+    prob = counts / len(ISIs)
+    bar(bins[:-1], prob, width=0.9)
+    lbda = 1 / ISIs.mean()
     model = lbda * exp(-lbda * bins) * 0.1
     plt.plot(bins, model, 'b')
     plt.xlabel('ISIs')
     plt.ylabel('Probability')
-    count=0
     # fig.savefig(r'C:\Users\User\OneDrive\Рабочий стол\bio_res\0'+str(count+1))
     # count+=1
     plt.show()
 
 data_0=[]
 x=0
-ISIs=[]
-a=[]
 for channel, items in enumerate(data):
     if channel==0:
         for era, varb in enumerate(items):
-            data_0.append(varb[0])
-            a=np.concatenate(data_0)
-        ISIs=np.diff(a)
-            # x += 1000
-analysis(ISIs)
+            data_0.append(varb[0]+x)
+            x+=1000
+data_0=np.concatenate(data_0)
+analysis(data_0)
