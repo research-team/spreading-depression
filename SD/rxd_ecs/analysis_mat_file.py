e95a4b570af292f91586193a4b43b68a7483127e
Kapoline <polina.chit@mail.ru>
2023-06-13 18:30:47 +0300

    update analysis

--- SD/rxd_ecs/analysis_mat_file.py
+++ SD/rxd_ecs/analysis_mat_file.py
@@ -17,34 +17,35 @@ def load_data(filepath):
     spks = mat['spks']
     return spks
 data = load_data(filepath)
-# print(data)
-
 color = ['red', 'blue', 'yellow', 'black', 'gray', 'cyan', 'magenta']
 
 fig, ax = plt.subplots()
-bins = np.arange(0.1, 100, 0.1)
+bins = np.arange(0, 10, 0.1)
 
 def analysis(spks):
-    arr=np.concatenate(spks)
-    ISIs=np.diff(arr)
+    ISIs=np.diff(spks)
     counts, _ = histogram(ISIs, bins)
     prob = counts / len(ISIs)
-    bar(bins[:-1], prob, width=0.9)
-    lbda = 1 / ISIs.mean()
-    model = lbda * exp(-lbda * bins) * 0.1
-    plt.plot(bins, model, 'b')
+    bar(bins[:-1], prob, width=0.1)
+    mu=ISIs.mean()
+    lbda= 1 / (1 / ISIs - 1 / mu).mean()
+    model=(
+    sqrt(lbda / 2 / pi / bins ** 3) *
+    exp(-lbda * (bins - mu) ** 2 / 2 / mu ** 2 / bins) * 0.1
+    )
+    model[0] = 0
+    # lbda = 1 / ISIs.mean()
+    # model = lbda * exp(-lbda * bins)
+    plt.plot(bins, model, 'b', color=color[0])
     plt.xlabel('ISIs')
     plt.ylabel('Probability')
-    # fig.savefig(r'C:\Users\User\OneDrive\Рабочий стол\bio_res\0'+str(count+1))
-    # count+=1
     plt.show()
 
 data_0=[]
 x=0
 for channel, items in enumerate(data):
-    if channel==0:
         for era, varb in enumerate(items):
             data_0.append(varb[0]+x)
             x+=1000
-data_0=np.concatenate(data_0)
+data_0=np.concatenate(data_0, axis=None)
 analysis(data_0)
