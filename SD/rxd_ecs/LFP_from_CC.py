f1b1a153264cdd7a27ffc7c7973ebabb5e097db7
Kapoline <polina.chit@mail.ru>
2023-12-05 19:40:59 +0300

    update calc lfp and ks lfp

--- SD/rxd_ecs/LFP_from_CC.py
+++ SD/rxd_ecs/LFP_from_CC.py
@@ -5,18 +5,18 @@ import matplotlib.pyplot as plt
 import csv
 import  math
 
-hdf5_files='D:/results1'
-coordinates_file='D:/results1/cord.csv'
+hdf5_files=''
+coordinates_file=''
 
 
 def load_voltages(path):
     vol_group = {}
-    voltages = []
     with os.scandir(path) as it:
         for entry in it:
             if entry.name.endswith(".hdf5") and entry.is_file():
+                file_name = os.path.splitext(entry.name)[0]
                 with h5py.File(entry.path, 'r') as f:
-                    vol_group[entry.name] = np.array(f[list(f.keys())[0][:]])
+                    vol_group[file_name] = np.array(f[list(f.keys())[0][:]])
     return vol_group
 
 def load_coordinates(path):
@@ -30,40 +30,47 @@ def load_coordinates(path):
 
 
 def calc_lfp(voltages, coords):
-    '''Допустим электрод находится на x = 100, y = 100, z = 100'''
-    Xe = 100
-    Ye = 100
-    Ze = 100
-    result = []
+    '''
+    Сенсоры на L23: 1, 2, 3
+    Сенсоры на L4: 4, 5, 6
+    Сенсоры на L5: 7, 8, 9
+    Сенсоры на L56: 10, 11
+    Сенсоры на L6: 12, 13, 14
+    Сенсоры на tf: 15, 16
+    '''
+    # sensors = {'1': [100, 100, -700], '2': [100, 100, -650], '3':[100, 100, -490], '4':[100, 100, -400], '5':[100, 100, -300], '6':[100, 100, -250],
+    #            '7':[100, 100, -100], '8':[100, 100, 50], '9': [100, 100, 100], '10':[100, 100, 250], '11':[100, 100, 330], '12':[100, 100, 400],
+    #            '13':[100, 100, 550], '14':[100, 100, 700], '15':[100, 100, 1120], '16':[100, 100, 1270]}
+    sensors = [[1, 100, 100, -700], [2, 100, 100, -650], [3, 100, 100, -490], [4, 100, 100, -400],
+               [5, 100, 100, -300], [6, 100, 100, -250], [7, 100, 100, -100], [8, 100, 100, 50],
+               [9, 100, 100, 100], [10, 100, 100, 250], [11, 100, 100, 330], [12, 100, 100, 400],
+               [13, 100, 100, 550], [14, 100, 100, 700], [15, 100, 100, 1120], [16, 100, 100, 1270]]
     fraction_dict = {}
-    for data in coords:
-        dkey = data[0]
-        x = float(data[1])
-        y = float(data[2])
-        z = float(data[3])
-        res = 0
-        for key, values in voltages.items():
-            vkey = key[0:30]
-            fraction_dict[vkey] = np.array(result)
-            if (21 <= len(key) < 27) and (dkey[0:21] == key[0:21]) :
-                square_of_distace = math.sqrt((Xe - x) ** 2 + (Ye - y) ** 2 + (Ze - z) ** 2)
-                for i in values:
-                    fraction = i / square_of_distace
-                    res += fraction
-                    result.append(res)
-                    result.append(fraction)
-            if (len(key) > 27) and (dkey[0:30] == vkey):
-                square_of_distace = math.sqrt((Xe - x) ** 2 + (Ye - y) ** 2 + (Ze - z) ** 2)
-                for i in values:
-                    fraction = i / square_of_distace
-                    res += fraction
-                    result.append(res)
-                    result.append(fraction)
+    for sensor in sensors:
+        sensor_number = sensor[0]
+        sX = sensor[1]
+        sY = sensor[2]
+        sZ = sensor[3]
+        result = []
+        for data in coords:
+            dkey = data[0]
+            x = float(data[1])
+            y = float(data[2])
+            z = float(data[3])
+            summ = 0
+            for key, value in voltages.items():
+                if key == dkey:
+                    square_of_distance = math.sqrt((sX - x) ** 2 + (sY - y) ** 2 + (sZ - z) ** 2)
+                    for i in value:
+                        fraction = i / square_of_distance
+                        summ+=fraction
+                    result.append(summ)
+        fraction_dict[str(sensor_number)] = np.array(result)
     return fraction_dict
-
 
 def calculation_simple_LFP(fraction):
     result = {}
+    res=[]
     const = 1 / (4 * math.pi)
     for key, value in fraction.items():
         lfp = const * value
@@ -77,7 +84,7 @@ def main():
     fraction = calc_lfp(voltages, coordinates)
     lfp = calculation_simple_LFP(fraction)
 
-    with h5py.File('D:/lfps/lfp3.hdf5', 'w') as hf:
+    with h5py.File('', 'w') as hf:
         group = hf.create_group('values')
         for key, value in lfp.items():
             group[key] = value
--- SD/rxd_ecs/ks_lfp.py
+++ SD/rxd_ecs/ks_lfp.py
@@ -2,13 +2,14 @@ import h5py
 import os
 import numpy as np
 import scipy
+from scipy import io
 from scipy.stats import kstwo, ks_2samp, kstest
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FixedLocator
 import statistics as st
 
-file = 'D:/lfps/lfp3.hdf5'
-matfile = r"C:\Users\User\OneDrive\Рабочий стол\2011 may 03 P32 BCX rust\2011_05_03_0003.mat"
+file = ''
+matfile = r""
 
 
 def load_cc_lfp(filepath):
@@ -35,11 +36,13 @@ def plotting_lfp(lfps):
     yx=1
     fig, ax = plt.subplots()
     keys = list(lfps.keys())
+    keys.sort()
     for i in keys:
         yx += 1
-        ax.plot(np.array(lfps[i]) + yx*(1e3-5))
-        y_ticks_list.append(i[7:20])
-    ax.set_yticklabels(y_ticks_list)
+        plt.plot(np.array(lfps[i]) + yx*(1e3-5))
+        # y_ticks_list.append(i[7:20])
+    plt.legend([key[7:20] for key in keys])
+    # ax.set_yticklabels(y_ticks_list)
     plt.show()
 
 
@@ -51,12 +54,26 @@ def main():
     mat_data = []
     mean_cc = 0
     mean_bio = 0
-    for i in lfp_mat:
-        mat_data = np.concatenate(i)
+    values = []
+
+    for channel, items in enumerate(lfp_mat):
+        for era, varb in enumerate(items):
+            mat_data.append(varb)
+            # mean_bio=st.mean(varb)
+
     for key, value in lfp_cc.items():
-        result = ks_2samp(value, mat_data)
-        mean_cc = st.mean(value)
+        values.append(value)
+        # mean_cc=st.mean(value)
+
+
+    result = ks_2samp(values[0], mat_data[0])
+
+    mat_data = np.concatenate(mat_data)
+    values = np.concatenate(values)
+
+    mean_cc = st.mean(values)
     mean_bio = st.mean(mat_data)
+
     print(result)
     print("Среднее значение в lfp_cc " + str(mean_cc))
     print("Среднее значение в lfp_mat " + str(mean_bio))
