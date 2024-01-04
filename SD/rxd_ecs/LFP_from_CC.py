e112cc4bbf952c5b650fd2163605ed78721f278e
Kapoline <polina.chit@mail.ru>
2024-01-04 12:03:40 +0300

    update lfp

--- SD/rxd_ecs/LFP_from_CC.py
+++ SD/rxd_ecs/LFP_from_CC.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import csv
 import  math
 
-hdf5_files=''
-coordinates_file=''
+hdf5_files='D:/new_res'
+coordinates_file='D:/results1/cord.csv'
 
 
 def load_voltages(path):
@@ -15,6 +15,7 @@ def load_voltages(path):
         for entry in it:
             if entry.name.endswith(".hdf5") and entry.is_file():
                 file_name = os.path.splitext(entry.name)[0]
+                file_name=file_name.split('_')[2]
                 with h5py.File(entry.path, 'r') as f:
                     vol_group[file_name] = np.array(f[list(f.keys())[0][:]])
     return vol_group
@@ -41,31 +42,32 @@ def calc_lfp(voltages, coords):
     # sensors = {'1': [100, 100, -700], '2': [100, 100, -650], '3':[100, 100, -490], '4':[100, 100, -400], '5':[100, 100, -300], '6':[100, 100, -250],
     #            '7':[100, 100, -100], '8':[100, 100, 50], '9': [100, 100, 100], '10':[100, 100, 250], '11':[100, 100, 330], '12':[100, 100, 400],
     #            '13':[100, 100, 550], '14':[100, 100, 700], '15':[100, 100, 1120], '16':[100, 100, 1270]}
-    sensors = [[1, 100, 100, -700], [2, 100, 100, -650], [3, 100, 100, -490], [4, 100, 100, -400],
-               [5, 100, 100, -300], [6, 100, 100, -250], [7, 100, 100, -100], [8, 100, 100, 50],
-               [9, 100, 100, 100], [10, 100, 100, 250], [11, 100, 100, 330], [12, 100, 100, 400],
-               [13, 100, 100, 550], [14, 100, 100, 700], [15, 100, 100, 1120], [16, 100, 100, 1270]]
+    sensors = [[1, 0, 0, -700], [2, 0, 0, -650], [3, 0, 0, -490], [4, 0, 0, -400],
+               [5, 0, 0, -300], [6, 0, 0, -250], [7, 0, 0, -100], [8, 0, 0, 50],
+               [9, 0, 0, 100], [10, 0, 0, 250], [11, 0, 0, 330], [12, 0, 0, 400],
+               [13, 0, 0, 550], [14, 0, 0, 700], [15, 0, 0, 1120], [16, 0, 0, 1270]]
     fraction_dict = {}
     for sensor in sensors:
+        arr = []
+        j = 0
         sensor_number = sensor[0]
         sX = sensor[1]
         sY = sensor[2]
         sZ = sensor[3]
-        result = []
-        for data in coords:
-            dkey = data[0]
-            x = float(data[1])
-            y = float(data[2])
-            z = float(data[3])
-            summ = 0
-            for key, value in voltages.items():
-                if key == dkey:
-                    square_of_distance = math.sqrt((sX - x) ** 2 + (sY - y) ** 2 + (sZ - z) ** 2)
-                    for i in value:
-                        fraction = i / square_of_distance
-                        summ+=fraction
-                    result.append(summ)
-        fraction_dict[str(sensor_number)] = np.array(result)
+        for coord in coords:
+            x = float(coord[1])
+            y = float(coord[2])
+            z = float(coord[3])
+            for k, i  in enumerate(voltages.get(coord[0])):
+                square_of_distance = math.sqrt((sX - x) ** 2 + (sY - y) ** 2 + (sZ - z) ** 2)
+                # voltages+=i
+                fraq = i / square_of_distance
+                if j==0:
+                    arr.append(fraq)
+                else:
+                    arr[k]+=fraq
+            j=1
+        fraction_dict[str(sensor_number)] = np.array(arr)
     return fraction_dict
 
 def calculation_simple_LFP(fraction):
@@ -84,8 +86,8 @@ def main():
     fraction = calc_lfp(voltages, coordinates)
     lfp = calculation_simple_LFP(fraction)
 
-    with h5py.File('', 'w') as hf:
-        group = hf.create_group('values')
+    with h5py.File('D:/lfps/lfp_sensors_new.hdf5', 'w') as hf:
+        group = hf.create_group('lfp')
         for key, value in lfp.items():
             group[key] = value
 
